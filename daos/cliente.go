6bab963dd92e4443024105ce8d605060c5d67da3
MiguelCiulog <angel.ciulog7@gmail.com>
2022-05-07 23:23:33 -0700

    initial commit

---
+++ daos/cliente.go
@@ -0,0 +1,122 @@
+package clientes
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// TODO: Make migrations work
+const (
+	DBName    = "clientes_actividad131"
+	Migration = "CREATE DATABASE IF NOT EXISTS `clientes_actividad13`;\n" +
+		"CREATE TABLE if not exists `clientes_actividad13.clientes`\n" +
+		"(`idclientes` INT NOT NULL AUTO_INCREMENT, " +
+		"`nombre` VARCHAR(45) NOT NULL, " +
+		"`email` VARCHAR(100) NULL, " +
+		"`fecha_nacimiento` DATE NOT NULL, " +
+		"PRIMARY KEY (`idclientes`));"
+)
+
+var db *sql.DB
+
+type Cliente struct {
+	IdClientes      int       `json:"idclientes"`
+	Nombre          string    `json:"nombre"`
+	Email           string    `json:"email"`
+	FechaNacimiento time.Time `json:"fechaNacimiento"`
+}
+
+func HelloWorld() {
+	fmt.Println("Hello world")
+}
+
+func InitializeConnectionDB() {
+	var err error
+	db, err = sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/clientes_actividad13?parseTime=true")
+
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(Migration)
+	// fmt.Println()
+	// _, err = db.Exec(Migration)
+
+	// if err != nil {
+	// 	fmt.Print(err.Error())
+	// }
+}
+
+func CloseDBConnection() {
+	defer db.Close()
+}
+
+func GetAllCliente() ([]Cliente, error) {
+	sql := "SELECT * FROM clientes"
+
+	results, err := db.Query(sql)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var clientes []Cliente
+	for results.Next() {
+		var cliente Cliente
+		err = results.Scan(&cliente.IdClientes, &cliente.Nombre, &cliente.Email, &cliente.FechaNacimiento)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		clientes = append(clientes, cliente)
+	}
+
+	return clientes, nil
+}
+
+func GetClienteByEmail(email string) (Cliente, error) {
+	sql := fmt.Sprintf(string(`SELECT * FROM clientes where email = "%s"`), email)
+
+	results, err := db.Query(sql)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var cliente Cliente
+	for results.Next() {
+		err = results.Scan(&cliente.IdClientes, &cliente.Nombre, &cliente.Email, &cliente.FechaNacimiento)
+		if err != nil {
+			panic(err.Error())
+		}
+
+	}
+
+	return cliente, nil
+}
+
+func AddCliente(cliente Cliente) error {
+	const sql = `INSERT INTO clientes(nombre, email, fecha_nacimiento) VALUES(?, ?, ?)`
+	_, err := db.Exec(sql, cliente.Nombre, cliente.Email, cliente.FechaNacimiento)
+	return err
+}
+
+func DeleteClienteByEmail(email string) error {
+	const sql = `DELETE FROM clientes WHERE email = (?);`
+	_, err := db.Exec(sql, email)
+	return err
+}
+
+func UpdateCliente(cliente Cliente) error {
+	sql := `UPDATE clientes SET
+		nombre = (?),
+		email = (?),
+		fecha_nacimiento = (?)
+		WHERE email = (?);`
+
+	_, err := db.Exec(sql, cliente.Nombre, cliente.Email, cliente.FechaNacimiento, cliente.Email)
+
+	return err
+}
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	cliente "github.com/MiguelCiulog/actividad-13/daos"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	r := setupRouter()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	cliente.InitializeConnectionDB()
+	r.Run(":8080")
+	cliente.CloseDBConnection()
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	// Get user by email
+	r.GET("/clientes/:email", func(c *gin.Context) {
+		email := c.Params.ByName("email")
+		fmt.Println(email)
+		clientes, err := cliente.GetClienteByEmail(email)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": err})
+		}
+
+		c.JSON(http.StatusOK, clientes)
+
+	})
+
+	// Get all
+	r.GET("/clientes", func(c *gin.Context) {
+		// user := c.Params.ByName("name")
+		clientes, err := cliente.GetAllCliente()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": err})
+		}
+
+		c.JSON(http.StatusOK, clientes)
+
+	})
+
+	// add
+	r.POST("/clientes/:email", func(c *gin.Context) {
+		email := c.Params.ByName("email")
+		var clienteRequest cliente.Cliente
+		clienteRequest.Email = email
+
+		c.ShouldBind(&clienteRequest)
+
+		err := cliente.AddCliente(clienteRequest)
+		// clientes, err := cliente.GetAllCliente()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": err})
+		}
+
+		c.JSON(http.StatusOK, `Client was added succesfully.`)
+	})
+
+	// update
+	r.PUT("/clientes/:email", func(c *gin.Context) {
+		email := c.Params.ByName("email")
+		var clienteRequest cliente.Cliente
+		clienteRequest.Email = email
+
+		c.ShouldBind(&clienteRequest)
+
+		err := cliente.UpdateCliente(clienteRequest)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": err})
+		}
+
+		c.JSON(http.StatusOK, `Client was updated succesfully.`)
+	})
+
+	// delete
+	r.DELETE("/clientes/:email", func(c *gin.Context) {
+		email := c.Params.ByName("email")
+
+		err := cliente.DeleteClienteByEmail(email)
+		// clientes, err := cliente.GetAllCliente()
+		if err != nil {
+			c.JSON(http.StatusMethodNotAllowed, gin.H{"Error": err})
+		}
+
+		c.JSON(http.StatusAccepted, `Client was deleted succesfully.`)
+	})
+
+	return r
+}
+
+func _() {
+	cliente.InitializeConnectionDB()
+	// fmt.Println(cliente.GetAllCliente())
+	// fmt.Println(cliente.GetClienteByEmail("Email1"))
+	var cl cliente.Cliente
+	cl.Nombre = "Nombre2"
+	cl.Email = "Email2"
+	cl.FechaNacimiento = time.Now()
+	fmt.Println(cliente.AddCliente(cl))
+	fmt.Println(cliente.DeleteClienteByEmail("Email2"))
+
+	cl.Nombre = "Nombre1"
+	cl.Email = "Email1"
+	fmt.Println(cliente.AddCliente(cl))
+
+	cl.Nombre = "nom"
+	fmt.Println(cliente.UpdateCliente(cl))
+
+	cliente.CloseDBConnection()
+}
